544e06a15ecbdaa1e5db8a8fd16c763f22c246d2
Di <diana8burkut@gmail.com>
2025-04-21 23:12:12 +0200

    створила проєкт у гітхаб, створила основні компоненти та рендеринг розмітки, додала стан

---
+++ goit-react-hw-02/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+import Description from './components/Description'
+import Options from './components/Options'
+import Feedback from './components/Feedback'
+function App() {
+  const [feedbacks, setfeedbacks] = useState({
+  good: 0,
+	neutral: 0,
+	bad: 0
+  })
+
+  return (
+    <>
+      <Description/>
+      <Options feedbacks={feedbacks} setfeedbacks={ setfeedbacks} />
+      <Feedback  feedbacks={feedbacks} />
+      </>
+    
+  )
+}
+
+export default App
